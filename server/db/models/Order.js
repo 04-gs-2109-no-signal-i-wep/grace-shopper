1e81f0b2e217e8545bb899f6c82dbc042bd0fbdc
Caroline Spiezio <spieziocaroline@gmail.com>
2021-11-11 11:42:20 -0500

    changes datatype from DATE to DATEONLY in Order model

--- server/db/models/Order.js
+++ server/db/models/Order.js
@@ -1,15 +1,15 @@
-const Sequelize = require("sequelize");
-const db = require("../db");
+const Sequelize = require('sequelize');
+const db = require('../db');
 
-const Order = db.define("order", {
+const Order = db.define('order', {
   order_total: Sequelize.INTEGER,
   total_quantity: Sequelize.INTEGER,
   is_completed: {
     type: Sequelize.BOOLEAN,
     defaultValue: false,
   },
-  date_placed: Sequelize.DATE,
-  date_shipped: Sequelize.DATE,
+  date_placed: Sequelize.DATEONLY,
+  date_shipped: Sequelize.DATEONLY,
 });
 
 module.exports = Order;
