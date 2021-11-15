d1cf4aad2bfaa31f6d86637a41c25d47d2855979
yillachen <chn.yilla@gmail.com>
2021-11-15 13:35:22 -0500

    style: navbar flex added

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { logout } from "../store";
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>Hearth</h1>
-    <nav>
+const Navbar = ({ handleClick, isLoggedIn }) => (
+  <nav>
+    <div className="mainNav">
+      <Link to="/home"><h1>hearth</h1></Link>
+      <Link to="/products">Products</Link>
+    </div>
+    <div className="sideNav">
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
+          <a href="#" onClick={handleClick}>Logout</a>
         </div>
       ) : (
         <div>
@@ -22,26 +22,25 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           <Link to="/signup">Sign Up</Link>
         </div>
       )}
-    </nav>
-    <hr />
-  </div>
-)
+    </div>
+  </nav>
+);
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
-    isLoggedIn: !!state.auth.id
-  }
-}
+    isLoggedIn: !!state.auth.id,
+  };
+};
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     handleClick() {
-      dispatch(logout())
-    }
-  }
-}
+      dispatch(logout());
+    },
+  };
+};
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(mapState, mapDispatch)(Navbar);
