67d368cdf5f6c8644a7836c56a7d4c52d192e381
Jean Chow <jeanchow@jeans-air.lan>
2021-11-16 13:46:53 -0800

    displays user data in a datagrid with edit and delete buttons

--- client/components/UserData.js
+++ client/components/UserData.js
@@ -3,6 +3,13 @@ import { connect } from "react-redux";
 import { fetchUsers } from "../store/users";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import List from "@mui/material/List";
+import { ListItem } from "@mui/material";
+import { ListItemText } from "@mui/material";
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { IconButton } from "@mui/material"
+import { Button } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export class UserData extends React.Component {
   componentDidMount() {
@@ -11,22 +18,34 @@ export class UserData extends React.Component {
 
   render() {
     const { allUsers } = this.props;
-    return (
-      <Container maxWidth="md" className="user-container">
-          <Grid container spacing={2} justifyContent="center" alignItems="center" direction="row">
-            {allUsers &&
-              allUsers.map((user) => {
-                return (
-                  <Grid item xs={8} md={4} key={user.id}>
-                    {user.id}
-                    {user.first_name}
-                    {user.is_admin ? "ADMIN" : "not admin"}
 
-                  </Grid>
-                );
-              })}
-          </Grid>
-      </Container>
+    const columns = [
+      { field: 'col1', headerName: 'ID', width: 50 },
+      { field: 'col2', headerName: 'First Name', width: 150 },
+      { field: 'col3', headerName: 'Last Name', width: 150 },
+      { field: 'col4', headerName: 'Email', width: 200 },
+      { field: 'col5', headerName: 'Admin Status', width: 150 },
+      { field: 'col6', headerName: '', width: 100, renderCell: () => {return <Button variant="outlined" size="small">Edit</Button>}},
+      { field: 'col7', headerName: '', width: 80, renderCell: () => {return <IconButton aria-label="delete"><DeleteIcon /></IconButton>}}
+
+    ];
+
+
+    const rows = allUsers.map((user) => ({
+      id: user.id,
+      col1: user.id,
+      col2: user.first_name,
+      col3: user.last_name,
+      col4: user.email_address,
+      col5: user.is_admin? "Admin":"Not Admin"
+    }));
+
+
+    return (
+      <div style={{ height: 600, width: '100%' }}>
+        <DataGrid rows={rows} columns={columns}/>
+      </div>
+
     );
   }
 }
