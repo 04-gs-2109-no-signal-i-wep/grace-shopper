10a4c70ee1f9cbc53c1f8cd69e8bf250e75eb648
Caroline Spiezio <spieziocaroline@gmail.com>
2021-11-16 16:02:39 -0500

    checking latest updates

--- client/store/cart.js
+++ client/store/cart.js
@@ -74,10 +74,10 @@ export const updateItemQuantity = (userId, productId, quantity) => {
 export const checkoutCart = (userId) => {
   return async (dispatch) => {
     try {
-      const { data: newCart } = await axios.put(
+      const { data: newOrderHistory } = await axios.put(
         `/api/orders/cart/checkout/${userId}`
       );
-      dispatch(setCart(newCart));
+      dispatch(setCart());
     } catch (error) {
       console.log('An error occurred in the checkoutCart thunk: ', error);
     }
